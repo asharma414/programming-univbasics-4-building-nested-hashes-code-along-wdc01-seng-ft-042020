9e9ec013c26d5e7493736cb3b7ccdcb7463b7474
Abhinav Sharma <abhi.sharm.414@gmail.com>
2020-04-15 05:01:02 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,8 @@ def bonus
    }
   }
 
-  #code your solution here:
-
-  
+puts epic_tragedy["montague"]["hero"]["status"]
+puts epic_tragedy["capulet"]["hero"]["status"]
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
